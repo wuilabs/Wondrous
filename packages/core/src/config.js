269f3136dea6acbd7ffcb1e45b56ffad859db35c
nebulisx <nebulisx@gmail.com>
2024-05-09 02:42:16 +0300

    add text, background, border colors to safelist

--- packages/core/src/config.js
+++ packages/core/src/config.js
@@ -1,6 +1,23 @@
 const colors = require('./colors')
 
-module.exports = config = {
+// Safelist
+const colourList = ['primary', 'neutral', 'danger', 'warning', 'success', 'info']
+const colorSafeList = []
+
+for (const colorName of colourList) {
+  const shades = [
+    50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950
+  ]
+
+  shades.map((shade) => {
+    colorSafeList.push(`text-${colorName}-${shade}`)
+    colorSafeList.push(`bg-${colorName}-${shade}`)
+    colorSafeList.push(`border-${colorName}-${shade}`)
+  })
+}
+
+module.exports = {
+  safelist: colorSafeList,
   theme: {
     colors: colors,
   }
